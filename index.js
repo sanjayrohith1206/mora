3330200ea79e8db579aa2b31ee1aaec61feaf0fc
sanjayrohith1206 <sanjayrohith1206@gmail.com>
2025-11-22 22:58:59 +0530

    Feat: base structure

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Server running...");
+const express = require("express");
+const app = express();
+
+const PORT = 3000; 
+
+app.get("/", (req, res) => {
+    res.send("Node server is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
